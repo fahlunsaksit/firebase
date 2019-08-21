eda0f47cff34926a45516a4b1a0fd2ead49d8bad
fahlunsaksit <saksit.ban@gmail.com>
2019-08-21 09:27:52 +0700

    fix subject go home and remove java code in app.js

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
-const firebase = require("firebase");
-// Required for side-effects
-require("firebase/firestore");
+var db = firebase.firestore();
 
+function saveData() {
+
+    db.collection("users").add({
+        firstName: document.getElementById('inputName').value,
+        lastName: document.getElementById('inputLastName').value
+    })
+        .then(function (docRef) {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(function (error) {
+            console.error("Error adding document: ", error);
+        });
+    alert('save data in firebase');
+    document.getElementById('inputName').value = '';
+    document.getElementById('inputLastName').value = '';
+}
+
+function readData() {
+    db.collection("users").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            console.log(`${doc.id} => ${doc.data().firstName}`);
+            var node = document.createElement('p');
+            var textNode = document.createTextNode('firstName: ' + doc.data().firstName + " " + 'lastName: ' + doc.data().lastName);
+            //var textNode = document.createTextNode("fah");
+            node.appendChild(textNode);
+            document.getElementById('showData').appendChild(node);
+
+        });
+    });
+}
