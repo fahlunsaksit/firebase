bdf876ff526b1fb1fd3f429b960df5e44ad1d979
fahlunsaksit <saksit.ban@gmail.com>
2019-08-27 19:44:55 +0700

    finish

--- appKongRukSiam.js
+++ appKongRukSiam.js
@@ -1,18 +1,43 @@
 const db = firebase.firestore();
+const form = document.querySelector('#formAdd');
 
-db.collection('users').get().then((snapshot)=>{
-    console.log(snapshot.docs);
-    snapshot.forEach(doc=>{
-      dataInTable(doc.data());
-    });
+db.collection('users').get().then((snapshot) => {
+  console.log(snapshot.docs);
+  snapshot.forEach(doc => {
+    dataInTable(doc);
+  });
 });
 
-function dataInTable(data) {
+function dataInTable(doc) {
   var table = document.getElementById('table');
   var row = table.insertRow(-1);
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
-  cell1.innerHTML = data.firstName;
-  cell2.innerHTML = data.lastName;
-  console.log(data);
+  var cell3 = row.insertCell(2);
+  cell2.innerHTML = doc.data().lastName;
+  cell1.innerHTML = doc.data().firstName;
+  let btn = document.createElement('button');
+  // btn.innerHTML = "<button class='btn btn-warning'>delete</button>";
+  btn.textContent = 'delete';
+  btn.setAttribute('class','btn btn-danger');
+  btn.setAttribute('data-id',doc.id);
+  cell3.appendChild(btn);
+
+  btn.addEventListener('click',(e)=>{
+    let id = e.target.getAttribute("data-id");
+
+    db.collection('users').doc(id).delete();
+  });
+  // console.log(data);
 }
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log(form.inputName.value + ' ' +form.inputLastName.value);
+  db.collection('users').add({
+    firstName:form.inputName.value,
+    lastName:form.inputLastName.value
+  });
+  form.inputName.innerHTML = '';
+  form.inputLastName.innerHTML = '';
+});
